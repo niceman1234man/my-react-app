244034d994c4d3559ffedfff09fc50a9a7be0a54
niceman1234man <yihunietarekegn18@gmail.com>
2024-07-31 16:28:01 +0300

    modified navbar

--- src/assets/components/Navbar.jsx
+++ src/assets/components/Navbar.jsx
@@ -1,21 +1,26 @@
-import React from 'react'
-import{AiOutlineClose,AiOutlineMenu} from 'react-icons/ai'
+import { useState } from 'react'
+import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai'
 
-function Navbar() {
+const Navbar = () => {
+  const [nav, setNav] = useState(false)
+
+  const handleNav = () => {
+    setNav(!nav)
+  }
+
   return (
     <div className='md:flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white'>
-        <h1 className='w-full text-3xl font-bold text-[#00df9a]'>REACT.</h1>
-        <ul className='md:flex '>
-            <li className='p-4 border-b-2'>Home</li>
-            <li className='p-4'>Campany</li>
-            <li className='p-4'>Resourses</li>
-            <li className='p-4'>About</li>
-            <li className='p-4'>Contact</li>
-        </ul>
-        <div className='flex  absolute top-2 right-4 md:hidden'>
-            <AiOutlineMenu size={20}/>
-
-        </div>
+      <h1 className='w-full text-3xl font-bold text-[#00df9a]'>REACT.</h1>
+      <ul className={`md:flex sm:w-[30%] ${nav ? 'block' : 'hidden'}`}>
+        <li className='p-4 border-b-2 border-gray-600 md:border-none'>Home</li>
+        <li className='p-4 border-b-2 border-gray-600 md:border-none'>Company</li>
+        <li className='p-4 border-b-2 border-gray-600 md:border-none'>Resources</li>
+        <li className='p-4 border-b-2 border-gray-600 md:border-none'>About</li>
+        <li className='p-4 border-b-2 border-gray-600 md:border-none'>Contact</li>
+      </ul>
+      <div className='flex absolute top-2 right-4 md:hidden' onClick={handleNav}>
+        {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+      </div>
     </div>
   )
 }
